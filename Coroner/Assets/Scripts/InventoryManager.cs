8c907658b011066d214dc2e7ac4b69dc6280ac29
Katrien Van Bauwel <Katrien.van.bauwel@hotmail.com>
2025-10-18 12:55:52 +0200

    Speler kan items oppikken en ze komen in een inventaris terecht

--- Coroner/Assets/Scripts/InventoryManager.cs
+++ Coroner/Assets/Scripts/InventoryManager.cs
@@ -1,29 +1,60 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
-    private List<string> collectedItems = new List<string>();
+
+    [Header("UI")]
+    public Transform inventoryPanel;      
+    public GameObject inventoryItemPrefab; 
+
+    private List<string> items = new List<string>();
 
     void Awake()
     {
-        if (Instance == null)
-            Instance = this;
-        else
-            Destroy(gameObject);
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
     }
 
     public void AddItem(string itemName)
     {
-        if (!collectedItems.Contains(itemName))
-        {
-            collectedItems.Add(itemName);
-        }
+        items.Add(itemName);
+        UpdateUI();
+        Debug.Log(itemName + " toegevoegd aan inventaris.");
     }
 
     public bool HasItem(string itemName)
     {
-        return collectedItems.Contains(itemName);
+        return items.Contains(itemName);
+    }
+
+    public void UseItem(string itemName)
+    {
+        if (items.Contains(itemName))
+        {
+            items.Remove(itemName);
+            UpdateUI();
+            Debug.Log(itemName + " gebruikt!");
+        }
+    }
+
+    private void UpdateUI()
+    {
+        if (inventoryPanel == null || inventoryItemPrefab == null) return;
+
+        
+        foreach (Transform child in inventoryPanel)
+            Destroy(child.gameObject);
+
+        
+        foreach (string item in items)
+        {
+            GameObject entry = Instantiate(inventoryItemPrefab, inventoryPanel);
+            var view = entry.GetComponent<InventoryItemView>();
+            if (view != null)
+                view.Bind(item);
+        }
     }
 }
--- Coroner/Assets/Scripts/PickUpItem.cs
+++ Coroner/Assets/Scripts/PickUpItem.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using TMPro;
 
 public class PickUpItem : MonoBehaviour
 {
-    public string itemName; 
+    public string itemName = "Tool";
+    public TextMeshProUGUI pickupText;
+    public float messageDuration = 2f;
+    public AudioClip pickupSound;
+    public float soundVolume = 0.8f;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        
+        if (pickupText == null)
+            pickupText = GameObject.Find("PickupMessage")?.GetComponent<TextMeshProUGUI>();
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            PlayPickupSound();
+            ShowPickupMessage("Je hebt een " + itemName + " gevonden!");
+            ShowPickupMessage(itemName + " toegevoegd aan de inventaris!");
             InventoryManager.Instance.AddItem(itemName);
-            Debug.Log(itemName + " toegevoegd aan inventaris!");
             gameObject.SetActive(false);
         }
+    }
+
+    void PlayPickupSound()
+    {
+        if (pickupSound != null)
+            audioSource.PlayOneShot(pickupSound, soundVolume);
+    }
+
+    void ShowPickupMessage(string message)
+    {
+        if (pickupText == null) return;
+
+        pickupText.text = message;
+        CancelInvoke(nameof(ClearMessage));
+        Invoke(nameof(ClearMessage), messageDuration);
+    }
+
+    void ClearMessage()
+    {
+        pickupText.text = "";
     }
 }
