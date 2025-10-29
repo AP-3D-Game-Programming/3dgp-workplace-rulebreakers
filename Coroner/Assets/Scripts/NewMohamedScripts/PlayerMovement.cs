c7cabf1cf6282d3472e994d92534819cbce39bf0
MohamedIkken <s158502@ap.be>
2025-10-29 16:38:02 +0100

    Add house, player footsteps and gravity

--- Coroner/Assets/Scripts/NewMohamedScripts/PlayerMovement.cs
+++ Coroner/Assets/Scripts/NewMohamedScripts/PlayerMovement.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 public class PlayerMovement : MonoBehaviour
 {
+    public AudioSource footstepAudio;
     public CharacterController controller;
     public float speed = 6.5f;
+    public float gravity = -9.81f * 2;
+
+    Vector3 velocity;
     // Update is called once per frame
     void Update()
     {
@@ -25,6 +30,20 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = right * x + forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        // Speel geluid af als de speler beweegt en op de grond staat
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S))
+        {
+            footstepAudio.enabled = true;
+        }
+        else
+        {
+            footstepAudio.enabled = false;
+        }
+
+        // zwaartkracht instellen
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
     }
 }
