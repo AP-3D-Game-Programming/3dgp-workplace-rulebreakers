40d9ac0d9ada163270c31876dccb036573d04a4d
jonasthijs-ap <jonas.thijs@student.ap.be>
2025-11-01 21:35:38 +0100

    Finalize basic corpse functionality

--- Coroner/Assets/Scripts/ExamineBodyPart.cs
+++ Coroner/Assets/Scripts/ExamineBodyPart.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class ExamineBodyPart : MonoBehaviour
 {
-    InventoryManager inventory;
+    private InventoryManager inventory;
+    public GameObject successPrefab;
+    public GameObject failPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,10 +22,12 @@ public class ExamineBodyPart : MonoBehaviour
 
         if (CompareTag(currentInstrument.tag))
         {
+            Instantiate(successPrefab, transform.position, transform.rotation, transform);
             Debug.Log($"CORRECT! {gameObject.name} was clicked with matching tag '{tag}'.");
         }
         else
         {
+            Instantiate(failPrefab, transform.position, Quaternion.identity);
             Debug.LogError("Wrong tag, try another one!");
         }
     }
