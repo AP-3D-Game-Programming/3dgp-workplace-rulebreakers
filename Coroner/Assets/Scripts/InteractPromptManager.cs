08c3c6e238280a75fa3b3c953902e64fc85cbcca
jonasthijs-ap <jonas.thijs@student.ap.be>
2025-11-09 21:59:09 +0100

    Add floating input icon as basis for interact prompt

---
+++ Coroner/Assets/Scripts/InteractPromptManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class InteractPromptManager : MonoBehaviour
+{
+    //public Sprite icon;
+    private Transform cam;
+    //public ShowInteractPrompt interactPrompt;
+
+    void Start()
+    {
+        //icon = interactPrompt.icon;
+        //gameObject.GetComponent<SpriteRenderer>().sprite = icon;
+        cam = Camera.main.transform;
+    }
+
+    void Update()
+    {
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
