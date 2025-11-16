bf77c3c03e326d9f2a5161a2b365cf9cfe880f5e
MohamedIkken <s158502@ap.be>
2025-11-16 16:09:34 +0100

    werkende level0

--- Coroner/Assets/Scripts/NewMohamedScripts/GameButtons.cs
+++ Coroner/Assets/Scripts/NewMohamedScripts/GameButtons.cs
@@ -1,12 +1,11 @@
-using System.Net.Sockets;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameButtons : MonoBehaviour
 {
-    public Button restartButton;
+    // public Button restartButton;
+    // public Button nextButton;
     public MouseMovement mouseMovement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,16 +15,21 @@ public class GameButtons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.inputString.ToLower() == "m")
-        {
-            mouseMovement.locked = !mouseMovement.locked;
-            Cursor.lockState = mouseMovement.locked ? CursorLockMode.Locked : CursorLockMode.None;
-            restartButton.gameObject.SetActive(true);
-        }
+        //if (Input.inputString.ToLower() == "m")
+        //{
+        //    mouseMovement.locked = !mouseMovement.locked;
+        //    Cursor.lockState = mouseMovement.locked ? CursorLockMode.Locked : CursorLockMode.None;
+        //    restartButton.gameObject.SetActive(true);
+        //}
     }
 
     public void RestatGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
     }
 }
